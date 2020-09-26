08ee530c175b6b20dae2be551922a60454f58de7
Administrator <admin@gmail.com>
2020-09-26 15:33:31 +0800

    注释了,for循环

--- hello.go
+++ hello.go
@@ -2,15 +2,16 @@ package main
 
 import "fmt"
 
-
+/*
 func loop_number() {
    for i :=0 ;i <=100; i++{
        fmt.Println(i)
    }
 }
+*/
 
 
 func main(){
-    loop_number()
+//    loop_number()
     fmt.Printf("Hello World !\n")
 }
