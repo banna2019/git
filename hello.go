7e9b4ad3ce3fe8e5612c2b7c09fb77bc420f8429
Administrator <admin@gmail.com>
2020-09-26 12:39:24 +0800

    更新golang代码

--- hello.go
+++ hello.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+
+func loop_number() {
+   for i :=0 ;i <=100; i++{
+       fmt.Println(i)
+   }
+}
+
+
 func main(){
+    loop_number()
     fmt.Printf("Hello World !\n")
 }
